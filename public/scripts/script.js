e1df3d2555c651ae2ce7f29cfd01562e4e75f5aa
rhit-meuniemj <meuniemj@rose-hulman.edu>
2025-04-04 13:46:02 -0400

    test

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,15 +6,13 @@ function resetColor(button) {
     button.className = 'linker-blue'; 
 }
 buttons.forEach(function(button) {
-    // Set initial color for each button
+
     button.className = "linker-blue"
 
-    // Add event listener for mouseover to change color to green
     button.addEventListener('mouseover', function() {
         button.className = 'linker-grey';
     });
 
-    // Add event listener for mouseout to reset color to blue
     button.addEventListener('mouseout', function() {
         resetColor(button)
     });
